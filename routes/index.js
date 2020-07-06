007e58b5158cf391efe678e7e18d3ee5b85d25fe
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-07-06 02:10:07 -0700

    fix bugs

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,22 @@ router.get("/", function(req, res){
 
 // show register form
 router.get("/register", function(req, res){
-   res.render("register"); 
+   res.render("register", {page: 'register'}); 
 });
 
 //handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
+    if(req.body.adminCode === process.env.ADMIN_CODE) {
+      newUser.isAdmin = true;
+    }
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            req.flash("error", err.message);
-            return res.render("register");
+            console.log(err);
+            return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Welcome to YelpCamp " + user.username);
+           req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
            res.redirect("/campgrounds"); 
         });
     });
@@ -30,24 +33,25 @@ router.post("/register", function(req, res){
 
 //show login form
 router.get("/login", function(req, res){
-   res.render("login"); 
+   res.render("login", {page: 'login'}); 
 });
 
 //handling login logic
 router.post("/login", passport.authenticate("local", 
     {
         successRedirect: "/campgrounds",
-        failureRedirect: "/login"
+        failureRedirect: "/login",
+        failureFlash: true,
+        successFlash: 'Welcome to YelpCamp!'
     }), function(req, res){
 });
 
 // logout route
 router.get("/logout", function(req, res){
    req.logout();
-   req.flash("success", "Logged you out!");
+   req.flash("success", "See you later!");
    res.redirect("/campgrounds");
 });
-
 
 
 module.exports = router;
