41aa1a790dda10031c9eea8a6070db33f9681bb7
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-07-06 10:53:15 -0700

    changes in routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require("express");
 var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
-var Notification = require("../models/notification");
-var { isLoggedIn } = require('../middleware');
 
 //root route
 router.get("/", function(req, res){
@@ -12,18 +10,22 @@ router.get("/", function(req, res){
 
 // show register form
 router.get("/register", function(req, res){
-   res.render("register"); 
+   res.render("register", {page: 'register'}); 
 });
 
 //handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
+    if(req.body.adminCode === process.env.ADMIN_CODE) {
+      newUser.isAdmin = true;
+    }
     User.register(newUser, req.body.password, function(err, user){
         if(err){
+            console.log(err);
             return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Welcome to YelpCamp " + user.username);
+           req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
            res.redirect("/campgrounds"); 
         });
     });
@@ -31,77 +33,25 @@ router.post("/register", function(req, res){
 
 //show login form
 router.get("/login", function(req, res){
-   res.render("login"); 
+   res.render("login", {page: 'login'}); 
 });
 
 //handling login logic
 router.post("/login", passport.authenticate("local", 
     {
         successRedirect: "/campgrounds",
-        failureRedirect: "/login"
+        failureRedirect: "/login",
+        failureFlash: true,
+        successFlash: 'Welcome to YelpCamp!'
     }), function(req, res){
 });
 
 // logout route
 router.get("/logout", function(req, res){
    req.logout();
-   req.flash("success", "Logged you out!");
+   req.flash("success", "See you later!");
    res.redirect("/campgrounds");
 });
-
-// user profile
-router.get('/users/:id', async function(req, res) {
-  try {
-    let user = await User.findById(req.params.id).populate('followers').exec();
-    res.render('profile', { user });
-  } catch(err) {
-    req.flash('error', err.message);
-    return res.redirect('back');
-  }
-});
-
-// follow user
-router.get('/follow/:id', isLoggedIn, async function(req, res) {
-  try {
-    let user = await User.findById(req.params.id);
-    user.followers.push(req.user._id);
-    user.save();
-    req.flash('success', 'Successfully followed ' + user.username + '!');
-    res.redirect('/users/' + req.params.id);
-  } catch(err) {
-    req.flash('error', err.message);
-    res.redirect('back');
-  }
-});
-
-// view all notifications
-router.get('/notifications', isLoggedIn, async function(req, res) {
-  try {
-    let user = await User.findById(req.user._id).populate({
-      path: 'notifications',
-      options: { sort: { "_id": -1 } }
-    }).exec();
-    let allNotifications = user.notifications;
-    res.render('notifications/index', { allNotifications });
-  } catch(err) {
-    req.flash('error', err.message);
-    res.redirect('back');
-  }
-});
-
-// handle notification
-router.get('/notifications/:id', isLoggedIn, async function(req, res) {
-  try {
-    let notification = await Notification.findById(req.params.id);
-    notification.isRead = true;
-    notification.save();
-    res.redirect(`/campgrounds/${notification.campgroundId}`);
-  } catch(err) {
-    req.flash('error', err.message);
-    res.redirect('back');
-  }
-});
-
 
 
 module.exports = router;
